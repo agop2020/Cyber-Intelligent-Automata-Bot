0d3abf59a59060afd4a481aa433b0b21e6aeabd5
Anish Gopalan <67121761+agop2020@users.noreply.github.com>
2020-09-18 14:31:01 -0700

    Removed flag checking for privacy reasons

--- main.py
+++ main.py
@@ -25,10 +25,7 @@ async def on_message(message):
     if message.content.find("!hello") != -1:
         await message.channel.send("Hi")
     if message.channel.type == discord.ChannelType.private: #the bot DMs the user & reads messages sent via DM. Note: message.channel.id is an int, not a string!
-        if message.content.find("Barcelona") != -1 or message.content.find("barcelona") != -1:
-            await message.channel.send("Correct! PART 2: You have found the city! Celebrities have been spotted attending hotels in Barcelona, such as the Hotel Casa Fuster. This may include someone who has worked with a certain movie production company only once, in 2016. **Message me the company name for your final OSINT clue. If you submit an incorrect answer, we will provide you with a hint.**")
-        elif message.content.find("Lionsgate") != -1 or message.content.find("lionsgate") != -1 or message.content.find("Lionsgate Corporation") != -1 or message.content.find("Lionsgate Studios") != -1:
-            await message.channel.send("Correct! PART 3: Well done! We are on the trail of the thief! Using your two previous answers(Lionsgate and Barcelona) and a certain French polyalphabetic cipher, obtain a string of text and google away with your result. Remember, github is your best friend! **This is it for the clues you will receive from me. The rest is up to you. Message the NSA bot to check your final flag.**")
+       #THIS PORTION OF THE CODE CONTAINS FLAG CHECKING - REMOVED FOR FLAG PRIVACY REASONS. 
         else:
             await message.channel.send(chat_bot(message.content))
 
